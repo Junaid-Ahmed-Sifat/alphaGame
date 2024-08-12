c1a7dca8bfc29f86d017450ade1364167dccae40
Junaid-Ahmed-Sifat <junaid.ahmed.sifat@g.bracu.ac.bd>
2024-08-13 02:27:43 +0600

    5th

--- scripts/alphaclash.js
+++ scripts/alphaclash.js
@@ -9,7 +9,26 @@
 
 // }
 
+function continueGame(){
+    const alpha = getARandomAlphabet();
+    console.log('your random alphabet', alpha)
+
+    // set randomly generated alphabet to the screen
+    const curAlphaElem = document.getElementById('current-alphabet');
+    curAlphaElem.innerText = alpha;
+
+    // set background color
+    setBackgroundColorById(alpha);
+}
+
 function play(){
     hideElementbyID('home-screen');
     showElementbyID('playgr')
+    continueGame();
+}
+
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add("bg-orange-300");
+
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -7,3 +7,19 @@ function showElementbyID(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
+
+function getARandomAlphabet(){
+    const alphastr= 'abcdefghijklmnopqrstuvwxyz'
+    const alphaarr = alphastr.split('');
+    console.log(alphaarr);
+
+    // get idx 
+
+    const randomnum = Math.random()*25;
+    const randomidx = Math.round(randomnum);
+    
+
+    const alpha = alphaarr[randomidx];
+    // console.log(randomidx, alpha);
+    return alpha;
+}
